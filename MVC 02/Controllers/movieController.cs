280e1e41328deb4dd72c9f393caf577b0b495dc1
Shimaa Elseidy <shimaaelseidy123@gmail.com>
2025-03-04 01:47:08 +0200

    03 Actions Return Types

--- MVC 02/Controllers/movieController.cs
+++ MVC 02/Controllers/movieController.cs
@@ -5,8 +5,70 @@ namespace MVC_02.Controllers
     // MVC Controller
     public class movieController : Controller
     {
-        // Action :: Public Non Static Functions
-        public string GetMovie(int id) => $"Get Movie By Id :: {id}";
+        // Action       :: Public Non Static Functions
+        // Actions      :: Have Special Return Type
+        // ActionResult :: 
+       // public string GetMovie(int id) => $"Get Movie By Id :: {id}";
         public string AddMovie(int id) => $"Add Movie By Id :: {id}";
+        //public ContentResult GetMovie(int id)
+        //{
+        //    var ContentResult = new ContentResult()
+        //    {
+        //        Content = $"Get Movie By Id :: {id}",
+        //        ContentType = "text/html",
+        //        StatusCode = 200
+        //    };
+        //    return ContentResult;
+        //}
+
+        //public ContentResult GetMovie(int id)
+        //{
+        //    var ContentResult = new ContentResult()
+        //    {
+        //        Content = $"Get Movie By Id :: {id}",
+        //        ContentType = "object/pdf",
+        //        StatusCode = 200
+        //    };
+        //    return ContentResult;
+        //}
+
+        //public RedirectResult GetMovie(int id)
+        //{
+        //    var RedirectResult = new RedirectResult("https://github.com/login");
+        //    return RedirectResult;
+        //}
+
+        //public RedirectToActionResult GetMovie(int id)
+        //{
+        //    var RedirectToActionResult = new RedirectToActionResult("AddMovie","movie",new { id = 122});
+        //    return RedirectToActionResult;
+        //}
+        [ActionName("GetMovie")]
+        //[HttpGet]
+        //[HttpPost]
+        public IActionResult GetMovieByIdAndCategoryDramaAndPoster(int id)
+        {
+           if (id == 0)
+           {
+               //var ContentResult = new ContentResult(){Content = $"Get Movie By Id :: {id}",ContentType = "text/html",StatusCode = 200};
+               return Content($"Get Movie By Id :: {id}", "text/html");
+           }
+            else if (id == 2)
+           {
+                //var ContentResult = new ContentResult() { Content = $"Get Movie By Id :: {id}", ContentType = "object/pdf", StatusCode = 200 };
+                return Content($"Get Movie By Id :: {id}", "object/pdf");
+           }
+           else if (id== 3)
+           {
+                //return new RedirectResult( "https://WWW.google.com/"); 
+                return Redirect("https://WWW.google.com/");
+           }
+           else
+           {
+                //return new RedirectToActionResult("AddMovie", "movie", new {id=33});
+                return RedirectToAction("AddMovie", "movie", new { id = 33 });
+            }
+
+        }
     }
 }
