ff1fb633ce9ef8e6bdde54d44e076f371cc7e3a7
Shimaa Elseidy <shimaaelseidy123@gmail.com>
2025-03-04 22:52:28 +0200

    05 Install Client Side Library -  Bootstrap

--- MVC 02/Program.cs
+++ MVC 02/Program.cs
@@ -13,19 +13,25 @@ namespace MVC_02
             //app.MapGet("/login", SignIn);
 
             // MVC 
+            //app.MapControllerRoute(
+            //    name: "Default" ,
+            //   pattern:"{controller}/{action}/{id?}"
+
+            //   // pattern: "{controller=movie}/{action=GetMovie}/{id=5}" // ==> Setting default values
+            //   // pattern: "{controller=movie}/{action=GetMovie}/{id:alpha=5}" // ==> Setting default values
+            //   // pattern: "{controller=movie}/{action=GetMovie}/{id:alpha:length(4)=5}" // ==> Setting default values
+            //   // pattern: "{controller=movie}/{action=GetMovie}/{id:alpha:length(3,6)=5}" // ==> Setting default values
+            //   // pattern: "{controller=movie}/{action=GetMovie}/{id:int=5}" // ==> Setting default values
+            //   // Regex ==> Regular Expression
+            //   // pattern: "{controller=movie}/{action=GetMovie}/{id:regex()=5}" // ==> Setting default values
+            //   //pattern: "{controller=movie}/{action=GetMovie}/{id?}" // ==> Setting default values ==> id --> optional not requaried
+            //    );
             app.MapControllerRoute(
-                name: "Default Routing" ,
-               // pattern:"{controller}/{action}/{id}"
-               // pattern: "{controller=movie}/{action=GetMovie}/{id=5}" // ==> Setting default values
-               // pattern: "{controller=movie}/{action=GetMovie}/{id:alpha=5}" // ==> Setting default values
-               // pattern: "{controller=movie}/{action=GetMovie}/{id:alpha:length(4)=5}" // ==> Setting default values
-               // pattern: "{controller=movie}/{action=GetMovie}/{id:alpha:length(3,6)=5}" // ==> Setting default values
-               // pattern: "{controller=movie}/{action=GetMovie}/{id:int=5}" // ==> Setting default values
-               // Regex ==> Regular Expression
-               // pattern: "{controller=movie}/{action=GetMovie}/{id:regex()=5}" // ==> Setting default values
-               pattern: "{controller=movie}/{action=GetMovie}/{id?}" // ==> Setting default values ==> id --> optional not requaried
-                );
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
             app.Run();
+            
         }
         public static string SignIn()
         {
